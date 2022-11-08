0bf3d8bdb09582c0830e13dcac7fa58786fd51c8
Gatien Couillebault <gatiencouillebault@3il.fr>
2022-11-08 15:28:04 +0100

    nav en plus

---
+++ nav.php
@@ -0,0 +1,11 @@
+<ul>
+  <li>
+  	<a href="services.pdf" class='lien'> Services </a>
+  </li>
+  <li>
+  	<a href="tarifs.php" class='lien'> Tarifs </a>
+  </li>
+  <li>
+  	<a href="contact.php" class='lien'> Contact </a>
+  </li>
+</ul>
