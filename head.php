3901e42475bb8714ab71d397622b463b53543016
Gatien Couillebault <gatiencouillebault@3il.fr>
2022-11-08 16:04:40 +0100

    Head

---
+++ head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>3iSystem</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header> <!-- A ne pas confondre avec <head> -->
+        <h1>3iSystem</h1>
+        <p>
+            <a href="https://www.3il-ingenieurs.fr/" target="_blank" class="lien">3iL Academy</a>
+        </p>
+    </header>
